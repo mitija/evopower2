4f91749687aab695b4549caac81b5b0b044d31e2
amelrnt <ochaarn@gmail.com>
2023-06-05 20:06:54 +0700

    progress FRD 1 and FRD 2

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+  'name' : 'evopower',
+  'version': '1.0',
+  'summary': 'Custom CRM App',
+  'depends': ['sale_crm'],  
+  'description' : 'EVO Power Application',
+  'data' : [
+      'views/evo_opportunity_report.xml',
+      'views/evo_opportunity_form.xml',
+      'views/evo_lost_reason.xml',
+  ]
+}
---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+from . import sale_opportunity, evo_product, evo_lost_reason
