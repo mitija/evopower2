7a78c4555867777009fb52329f563fab8729f8b1
ameliarnt <amelia@mplus.software>
2023-09-26 22:08:25 +0700

    +T15295 fix error

--- evopower/models/account_move_line.py
+++ evopower/models/account_move_line.py
@@ -6,13 +6,29 @@ class AccountMoveLine(models.Model):
 
   @api.depends('sale_line_ids.order_id.manual_currency_rate', 'purchase_line_id.order_id.manual_currency_rate')
   def _compute_currency_rate(self):
+      def get_rate(from_currency, to_currency, company, date):
+          return self.env['res.currency']._get_conversion_rate(
+              from_currency=from_currency,
+              to_currency=to_currency,
+              company=company,
+              date=date,
+          )
       for line in self:
           if line.currency_id:
-              if line.journal_id.type == 'sale':
+              if (line.journal_id.type == 'sale' 
+                  and line.sale_line_ids.order_id.manual_currency_rate > 0):
                 line.currency_rate = line.sale_line_ids.order_id.manual_currency_rate
-              elif line.journal_id.type == 'purchase':
+              elif (line.journal_id.type == 'purchase' 
+                    and line.purchase_line_id.order_id.manual_currency_rate > 0):
                 line.currency_rate = line.purchase_line_id.order_id.manual_currency_rate
               else:
-                line.currency_rate = 1
+                line.currency_rate = get_rate(
+                  from_currency=line.company_currency_id,
+                  to_currency=line.currency_id,
+                  company=line.company_id,
+                  date=line.move_id.invoice_date 
+                  or line.move_id.date 
+                  or fields.Date.context_today(line),
+              )
           else:
               line.currency_rate = 1
